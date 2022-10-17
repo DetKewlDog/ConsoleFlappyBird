6307b19ef97d8ef1d0bb85d1c11d2d38863d803a
Bar Goldenstein <bargolden12@gmail.com>
2022-10-17 13:32:28 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFlappyBird
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
